9a745106ef3d6b8e0c6567fe05833554993a22b0
Luskahz <lucasalvesdemelolins@gmail.com>
2025-05-27 20:10:09 -0300

    alteração no schema

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import alunoRoutes from './routers/alunoRoutes.js'
 import periodoRouter from './routers/periodoRouter.js'
 import cursoRouter from './routers/cursoRouter.js'
 import professorRoutes from './routers/professorRouter.js';
+import notaRouter from './routers/notaRouter.js'
 
 // Carrega variáveis de ambiente do arquivo .env
 dotenv.config();
@@ -24,7 +25,8 @@ app.get('/', welcomeController);
 app.use('/alunos', alunoRoutes);
 app.use('/periodo', periodoRouter);
 app.use('/curso', cursoRouter);
-app.use('/professores', professorRoutes); 
+app.use('/professores', professorRoutes);
+app.use('/nota', notaRouter)
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
